afbe1ca6ad682c9d6232d3c570f2fd1d31cb77fd
James Kinyua <james.kinyua@agilebiz.co.ke>
2025-05-23 13:43:07 +0300

    Make `Year` column required with default value in migration
    Updated the `ManagementReturns.Year` column to be non-nullable
    and set a default value of an empty string. Added a new
    migration class `SomePendingThings` to handle this change.
    Updated `ReturnsDbContextModelSnapshot` to reflect the new
    `Year` property as required with a column type of `nvarchar(max)`.
    Included `Up` and `Down` methods in the migration to ensure
    reversible changes. Applied namespace and metadata updates
    in the migration file.

---
+++ Returns/Migrations/20250523104232_SomePendingThings.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Returns.Migrations
+{
+    /// <inheritdoc />
+    public partial class SomePendingThings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Year",
+                table: "ManagementReturns",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Year",
+                table: "ManagementReturns",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
